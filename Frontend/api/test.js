4d3c8fb30b492067072c8fb6a430395f68d18672
saurabh97858 <saurabhgupta97858@gmail.com>
2026-05-04 14:21:46 +0530

    Clean up Frontend for pure Vercel SPA deployment

--- Frontend/api/test.js
+++
@@ -1,3 +0,0 @@
-export default (req, res) => {
-  res.status(200).send('Vercel Function Test Successful');
-};
--- Frontend/server/routes/experienceRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createExperience, getAllExperiences } from '../controllers/experienceController.js';
-
-const experienceRouter = express.Router();
-
-experienceRouter.post('/create', createExperience);
-experienceRouter.get('/all', getAllExperiences);
-
-export default experienceRouter;
